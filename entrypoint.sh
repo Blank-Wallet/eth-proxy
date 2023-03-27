baf675b2529c4cee1daa6e71c129cef0b846824c
Leandro <leandro@blockwallet.io>
2023-03-27 16:13:49 -0300

    refactor: auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,49 +1,18 @@
 #!/bin/bash
 set -eu
 
-export DNS="${DNS}"
 export PORT="${RPC_PORT:-80}"
-export WS="${WS:-"NO"}"
 export STATUS_METHOD="${STATUS_METHOD:-net_version}"
 export AUTHORIZATION_HEADER="${AUTHORIZATION_HEADER:-}"
+export AUTHORIZATION_HEADER_VALUE="${AUTHORIZATION_HEADER_VALUE:-}"
+export AUTHORIZATION=""
 
-if [[ ! -z "${WS_ENDPOINT:-}"  ]]; then
-    export WS="YES"
+if [[ ! -z "${AUTHORIZATION_HEADER:-}" ]] && [[ ! -z "${AUTHORIZATION_HEADER_VALUE:-}" ]]; then
+    AUTHORIZATION=$(echo -n "proxy_set_header ${AUTHORIZATION_HEADER} ${AUTHORIZATION_HEADER_VALUE};")    
 fi
-
-if [[ ! -z "${ENDPOINT:-}"  ]]; then
-    export RPC_ENDPOINT="${ENDPOINT}"
-    export WS_ENDPOINT="${ENDPOINT}"
-else
-    export RPC_ENDPOINT="${RPC_ENDPOINT}"
-    export WS_ENDPOINT="${WS_ENDPOINT}"
-fi
-
-if [[ -z "${USERNAME:-}" || -z "${PASSWORD:-}" ]] && [[ "$WS" = "YES" ]]; then
-    # WS and no auth
-    file=/tmp/rpc.ws.conf.template
-    #envsubst '${AUTHORIZATION_HEADER}' < /tmp/rpc.ws.conf.template > /etc/nginx/conf.d/default.conf
-elif [[ -z "${USERNAME:-}" || -z "${PASSWORD:-}" ]]; then
-    # no ws and no auth
-    file=/tmp/rpc.conf.template
-    #envsubst '${AUTHORIZATION_HEADER}' < /tmp/rpc.conf.template > /etc/nginx/conf.d/default.conf
-else
-    AUTHORIZATION=$(echo -n "${USERNAME}:${PASSWORD}" | base64 | tr -d \\n)
-    AUTHORIZATION_HEADER=$(echo -n "proxy_set_header Authorization \"Basic ${AUTHORIZATION}\";")    
-    if [[ "$WS" = "YES" ]]; then
-        # ws and auth
-        file=/tmp/rpc.ws.conf.template
-        #envsubst '${AUTHORIZATION_HEADER}' < /tmp/rpc.ws.conf.template > /etc/nginx/conf.d/default.conf
-    else
-        # no ws and auth
-        file=/tmp/rpc.conf.template
-        #envsubst '${AUTHORIZATION_HEADER}' < /tmp/rpc.conf.template > /etc/nginx/conf.d/default.conf
-    fi
-fi
-envsubst '${AUTHORIZATION_HEADER}' < $file > /etc/nginx/conf.d/default.conf
-envsubst '${DNS} ${PORT}' < /tmp/common.template  > /etc/nginx/conf.d/common.template 
+mv /tmp/rpc.conf.template /etc/nginx/conf.d/default.conf
+envsubst '${PORT}' < /tmp/common.template  > /etc/nginx/conf.d/common.template 
 envsubst '${STATUS_METHOD}' < /tmp/status.template  > /etc/nginx/conf.d/status.template 
-envsubst '${RPC_ENDPOINT}' < /tmp/rpc.template  > /etc/nginx/conf.d/rpc.template 
-envsubst '${WS_ENDPOINT}' < /tmp/ws.template  > /etc/nginx/conf.d/ws.template 
+envsubst '${AUTHORIZATION} ${ENDPOINT}' < /tmp/rpc.template  > /etc/nginx/conf.d/rpc.template 
 
 exec "$@"
